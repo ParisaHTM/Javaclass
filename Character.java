32ab0d7d838fb7f20e0600ee549fb94310405378
ParisaHTM <92890713+ParisaHTM@users.noreply.github.com>
2022-04-08 16:08:05 -0400

    update

--- Character.java
+++ Character.java
@@ -21,7 +21,7 @@ public class Character
     private int strength;
     private int dexterity;
     private int constitution;
-    private int Intelligence;
+    private int intelligence;
     private int wisdom;
     private int charisma;
     
@@ -31,12 +31,13 @@ public class Character
     
     public int chooseMethod1()
     {
+        sum = 0;
         Random rand = new Random();
         for(int a = 0; a<4; a++)
         {                
-            int i = rand.nextInt(5)+1;
+            int i = rand.nextInt(6)+1;
             sum = sum + i;               
-            if (sum < 3 & sum > 18)
+            if (sum < 3 || sum > 18)
             {
                 continue;
             }                
@@ -48,20 +49,21 @@ public class Character
     public int chooseMethod2()
     {
         sum = 0;
-        while (sum<3 & sum> 18)
+        while(sum == 0)
         {
-            for(int s = 0; s<7; s++)
+            for(int s = 0; s<7; s++) // for abilities
             {
+                sum = 0;
                 Random rand = new Random();
-                int[] number = new int[5];
+                int[] number = new int[5]; //for choosing 5 number
                 
-                for (int b = 0; b<6; b++)
+                for (int b = 0; b<5; b++)  //for choosing 5 number
                 {
-                    int i = rand.nextInt(5)+1;
+                    int i = rand.nextInt(6)+1;
                     number[b] = i;
                 }
 
-                for (int c = 0; c<4; c++)
+                for (int c = 0; c<4; c++)  //for choosing 3 of them for summing
                 {        
                     int max_num = number[0];
                     int e = 0;
@@ -76,8 +78,14 @@ public class Character
                     //maxArray[c] = max_num;
                     number[e] = 0;
                     sum = sum + max_num;
+                    //System.out.print("sum = "+ sum + "\n");
                 
                 }
+            }
+            if (sum < 3 || sum > 18)
+            {
+                sum = 0;
+                continue;
             }
         }                    
         return sum;
@@ -87,18 +95,19 @@ public class Character
     {
         sum = 0;
             
-        while (sum<4 & sum> 21)
+        while (sum == 0)
         {
             for(int s = 0; s<7; s++)
             {
+                sum = 0;
                 Random rand = new Random();
                 int[] number = new int[5];
                 //int[] maxArray = new int[3];
                 //ArrayList<Integer> number = new ArrayList<Integer> ();
 
-                for (int b = 0; b<6; b++)
+                for (int b = 0; b<5; b++)
                 {
-                    int i = rand.nextInt(5)+1;
+                    int i = rand.nextInt(6)+1;
                     number[b] = i;
                 }
 
@@ -119,14 +128,14 @@ public class Character
                     //maxArray[c] = max_num;
                     Random plus = new Random();
                     int[] plusNumber = new int[3];                    
-                    for (int f = 0; f<4; f++)
+                    for (int f = 0; f<3; f++)
                     {
                         int j = plus.nextInt(3)+1;
                         plusNumber[f] = j;
                     }
 
                     int maxPlus = plusNumber[0];
-                    for(int d = 1; d<number.length; d++)
+                    for(int d = 1; d<plusNumber.length; d++)
                     {
                         if(plusNumber[d]>max_num)
                         {
@@ -134,7 +143,13 @@ public class Character
                         }                    
                     }                   
                     sum = sum + max_num+ maxPlus;
+                    //System.out.print("sum = "+ sum + "\n");
                 }
+            }
+            if (sum < 3 || sum > 18)
+            {
+                sum = 0;
+                continue;
             }
         }
         return sum;        
@@ -151,7 +166,7 @@ public class Character
             this.strength = chooseMethod1();
             this.dexterity = chooseMethod1();
             this.constitution = chooseMethod1();
-            this.Intelligence = chooseMethod1();
+            this.intelligence = chooseMethod1();
             this.wisdom = chooseMethod1();
             this.charisma = chooseMethod1();  
         }
@@ -160,7 +175,7 @@ public class Character
             this.strength = chooseMethod2();
             this.dexterity = chooseMethod2();
             this.constitution = chooseMethod2();
-            this.Intelligence = chooseMethod2();
+            this.intelligence = chooseMethod2();
             this.wisdom = chooseMethod2();
             this.charisma = chooseMethod2(); 
         }
@@ -170,13 +185,13 @@ public class Character
             this.strength = chooseMethod3();
             this.dexterity = chooseMethod3();
             this.constitution = chooseMethod3();
-            this.Intelligence = chooseMethod3();
+            this.intelligence = chooseMethod3();
             this.wisdom = chooseMethod3();
             this.charisma = chooseMethod3();             
             
-        }  
-    }
-    public String displayScores(){
-        
+        } 
+        System.out.print("Strength = "+ strength+ "\n"+ "Dexterity = "+ dexterity +"\n"+
+                "Constitution = "+ constitution+"\n"+"Intelligence = " + intelligence + "\n"+
+                "Wisdom = "+ wisdom+"\n"+ "Charisma = "+ charisma+ "\n");
     }
 }
